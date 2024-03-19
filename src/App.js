f39affd4341e165e3ba8c2723df5adff79fcb881
denizAM <deniz.a.marin@gmail.com>
2024-03-19 00:12:56 -0600

    1.0 Conexión Studio Code - GitHub

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Sitio oficial <code>src/App.js</code> de Santa Ana Coatepec.
         </p>
         <a
           className="App-link"
